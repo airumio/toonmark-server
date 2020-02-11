b32a8c3e9963f53c2c85101f238183ef773bc03c
airumio <airumio@naver.com>
2020-02-12 00:13:23 +0900

    3rd commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/crawling", (req : Request, res : Response) => {
+    res.send("hello");
+    
+});
+
+app.listen(8080, () => {
+
+});
+
