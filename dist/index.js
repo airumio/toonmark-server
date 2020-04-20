24bea07af747252a7fa23e35ba855a243b2c9e44
airumio <airumio@naver.com>
2020-04-21 01:27:23 +0900

    fix bug

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,7 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const routing_controllers_1 = require("routing-controllers");
@@ -6,41 +9,25 @@ const typedi_1 = require("typedi");
 const WebtoonController_1 = require("./controller/WebtoonController");
 const LoggingMiddleware_1 = require("./middleware/LoggingMiddleware");
 const TestInterceptor_1 = require("./interceptor/TestInterceptor");
+const fs_1 = __importDefault(require("fs"));
+const spdy_1 = __importDefault(require("spdy"));
+const certPath = `${__dirname}\\..\\.well-known\\validation`;
 routing_controllers_1.useContainer(typedi_1.Container);
+const cert = {
+    key: fs_1.default.readFileSync(`${certPath}\\private.key`, 'utf8'),
+    cert: fs_1.default.readFileSync(`${certPath}\\self-signed.crt`, 'utf8'),
+    passphrase: 'campantan',
+};
 const app = routing_controllers_1.createExpressServer({
     controllers: [WebtoonController_1.WebtoonController],
     middlewares: [LoggingMiddleware_1.LoggingMiddleware],
     interceptors: [TestInterceptor_1.TestInterceptor],
 });
-app.listen(8080, () => {
+const app2 = spdy_1.default.createServer(cert, app);
+app.listen(80, () => {
     console.log('server on~');
 });
-// import express, { Request, Response } from 'express';
-// import Cheerio, { FetchResult } from 'cheerio-httpcli';
-// import Sample from './Sample';
-// const app = express();
-// const url: string = 'https://comic.naver.com/webtoon/weekday.nhn';
-// const param = {};
-// app.get('/crawling', (req: Request, res: Response) => {
-//   Cheerio.fetch(url, param)
-//     .then((result) => {
-//       res.send(result.body);
-//       // result.$("a").each(() => console.log(this.Text));
-//       result.$('a').each((idx, el) => {
-//         console.log(el.attribs.href);
-//       });
-//     })
-//     .catch((err) => {
-//       res.send(err);
-//     });
-// });
-// app.get('/webtoon/:platform', (req: Request, res: Response) => {
-//   console.log(req.params);
-//   console.log(req.query.weekday);
-//   Sample(req, res);
-//   console.log('!!');
-// });
-// app.listen(8080, () => {
-//   console.log('server on');
-// });
+app2.listen(443, () => {
+    console.log('https server on~');
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,18 @@ import { Container } from 'typedi';
 import { WebtoonController } from './controller/WebtoonController';
 import { LoggingMiddleware } from './middleware/LoggingMiddleware';
 import { TestInterceptor } from './interceptor/TestInterceptor';
+import fs from 'fs';
+import spdy from 'spdy';
+
+const certPath = `${__dirname}\\..\\.well-known\\validation`;
 
 useContainer(Container);
+
+const cert = {
+  key: fs.readFileSync(`${certPath}\\private.key`, 'utf8'),
+  cert: fs.readFileSync(`${certPath}\\self-signed.crt`, 'utf8'),
+  passphrase: 'campantan',
+};
 
 const app = createExpressServer({
   controllers: [WebtoonController],
@@ -13,39 +23,12 @@ const app = createExpressServer({
   interceptors: [TestInterceptor],
 });
 
-app.listen(8080, () => {
+const app2 = spdy.createServer(cert, app);
+
+app.listen(80, () => {
   console.log('server on~');
 });
 
-// import express, { Request, Response } from 'express';
-// import Cheerio, { FetchResult } from 'cheerio-httpcli';
-// import Sample from './Sample';
-
-// const app = express();
-// const url: string = 'https://comic.naver.com/webtoon/weekday.nhn';
-// const param = {};
-
-// app.get('/crawling', (req: Request, res: Response) => {
-//   Cheerio.fetch(url, param)
-//     .then((result) => {
-//       res.send(result.body);
-//       // result.$("a").each(() => console.log(this.Text));
-//       result.$('a').each((idx, el) => {
-//         console.log(el.attribs.href);
-//       });
-//     })
-//     .catch((err) => {
-//       res.send(err);
-//     });
-// });
-
-// app.get('/webtoon/:platform', (req: Request, res: Response) => {
-//   console.log(req.params);
-//   console.log(req.query.weekday);
-//   Sample(req, res);
-//   console.log('!!');
-// });
-
-// app.listen(8080, () => {
-//   console.log('server on');
-// });
+app2.listen(443, () => {
+  console.log('https server on~');
+});
