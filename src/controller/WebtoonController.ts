15618f1ed399564a9890d3f9fc196c4150439e35
airumio <airumio@naver.com>
2020-04-22 04:20:39 +0900

    fix search func

--- src/controller/WebtoonController.ts
+++ src/controller/WebtoonController.ts
@@ -6,7 +6,11 @@ import Moment from 'moment';
 import { Controller, JsonController, Param, Get } from 'routing-controllers';
 import { BaseController } from './BaseController';
 import { Platform, Weekday } from '../model/Enum';
-import { platformDaytype, weekDayKorToEng } from '../model/Object';
+import {
+  platformDaytype,
+  platformKorToEng,
+  weekDayKorToEng,
+} from '../model/Object';
 import { config } from '../config/config';
 import {
   BaseService,
@@ -217,6 +221,29 @@ export class WebtoonController extends BaseController {
     }
   }
 
-  @Get(`/:platform(${platformregex})/:criteria`)
-  public async getSearchData() {}
+  @Get(`/search/:criteria`)
+  public async getSearchData(@Param('criteria') criteria: string) {
+    const dataList = fs.readdirSync(dataPath);
+
+    const data = dataList.reduce((prev, cur) => {
+      return prev.concat(
+        JSON.parse(
+          fs.readFileSync(
+            path.join(dataPath, cur, `${cur}_all${config.dataType}`),
+            'utf8',
+          ),
+        ),
+      );
+    }, []);
+
+    const result = data.filter((val: IwebtoonDTO) => {
+      if (val.title.includes(criteria)) return true;
+      if (val.author.includes(criteria)) return true;
+      if (val.genre.includes(criteria)) return true;
+      if (val.platform.includes(criteria)) return true;
+      if (val.platform.includes(platformKorToEng[criteria])) return true;
+    });
+
+    return result;
+  }
 }
